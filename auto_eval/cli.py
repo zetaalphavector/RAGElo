e0a618cf964e5dff437634ef4166d904692b16fb
Arthur Camara <camara@zeta-alpha.com>
2023-10-12 11:40:46 +0200

    Elo ranking

--- auto_eval/cli.py
+++ auto_eval/cli.py
@@ -1,10 +1,13 @@
 from typing import Optional
 
 import typer
+from loguru import logger
+from rich import print
 from typing_extensions import Annotated
 
 from auto_eval import __app_name__, __version__
 from auto_eval.documents_eval import DocumentEvaluator
+from auto_eval.elo_ranker import TournamentEloRating
 from auto_eval.games_creator import GamesCreator
 from auto_eval.games_runner import GamesRunner
 
@@ -133,14 +136,61 @@ def run_games(
 ):
     """Uses an Evaluator to evaluate a series of pairwise games with prompts created by the create-pairwise-games command."""
     runner = GamesRunner(
-        games_file, output_file, model_name, credentials_file, print_answers, force
+        games_file,
+        output_file,
+        answers_file,
+        model_name,
+        credentials_file,
+        print_answers,
+        force,
     )
     runner.run_games()
 
 
 @app.command()
-def evaluate_games():
-    pass
+def evaluate_games_elo(
+    games_file: Annotated[
+        str,
+        typer.Argument(
+            help="csv file with games to evaluate. "
+            "Each row must have agent_a, agent_b, score [0, 0.5, 1]"
+        ),
+    ],
+    output_file: Annotated[str, typer.Argument(help="csv file to write results to")],
+    k: Annotated[
+        int,
+        typer.Argument(
+            help="Elo ranking K parameter, maximum score to lose "
+            "(or win) in a single game"
+        ),
+    ] = 100,
+    intitial_score: Annotated[
+        int, typer.Argument(help="initial score for each agent")
+    ] = 1000,
+    rounds: Annotated[int, typer.Argument(help="Number of rounds to play")] = 10,
+    print_results: Annotated[
+        bool, typer.Option("--print", help="Print ranking after each iteration")
+    ] = False,
+):
+    """Evaluate all ran games using a Elo ranking system"""
+    for round in range(rounds):
+        with open(output_file, "a") as f:
+            f.write(f"Round {round}\n")
+        logger.debug(f"Starting round {round}")
+        if print_results:
+            print(f":crossed_swords: Starting round {round}")
+
+        ranker = TournamentEloRating(games_file, k, intitial_score, print_results)
+        ranker.play_all_games()
+        for player, rating in sorted(
+            tournament.get_player_ratings().items(), key=lambda x: x[1], reverse=True
+        ):
+            if print_results:
+                print(f"[bold white]{player:<15}[/bold white]: [{rating:.1f<6}")
+            with open(output_file, "a") as f:
+                f.write(f"{player},{rating}\n")
+        with open(output_file, "a") as f:
+            f.write("-" * 20 + "\n")
 
 
 @app.command()
