ab74f5200d6219ae37b272a56a7836d306797fc1
Arthur Camara <camara@zeta-alpha.com>
2023-10-16 17:08:00 +0200

    removed loguru and rich where not needed

--- auto_eval/logger.py
+++ auto_eval/logger.py
@@ -5,11 +5,11 @@ from auto_eval import __app_name__
 logger = logging.getLogger(__app_name__)
 
 
-class CLILogger(logging.Logger):
-    """A fake "logger" for the CLI use case"""
+class CLILogHandler(logging.Handler):
+    """A logging handler for the CLI use case"""
 
-    def __init__(self, name: str = __app_name__):
-        super().__init__(name)
+    def __init__(self):
+        super().__init__()
         try:
             import rich
 
@@ -19,30 +19,19 @@ class CLILogger(logging.Logger):
         except ImportError:
             self.rich = False
 
-    def warning(self, msg: str):
-        lvl_name = "WARNING"
-        if self.rich:
-            self.print(f"[yellow]{msg}[/yellow]")
-        else:
-            self.print(f"{lvl_name:<8}| {msg}")
-
-    def debug(self, msg: str):
-        lvl_name = "DEBUG"
-        if self.rich:
-            self.print(f"[blue bold]{msg}[/blue bold]")
-        else:
-            self.print(f"{lvl_name:<8}| {msg}")
-
-    def info(self, msg: str):
-        lvl_name = "INFO"
-        if self.rich:
-            self.print(msg)
-        else:
-            self.print(f"{lvl_name:<8}| {msg}")
-
-    def error(self, msg: str):
-        lvl_name = "ERROR"
-        if self.rich:
-            self.print(f"[bold red]{msg}[/bold red]")
-        else:
-            self.print(f"{lvl_name:<8}| {msg}")
+    def emit(self, record):
+        try:
+            msg = self.format(record)
+            if self.rich:
+                if record.levelno == logging.WARNING:
+                    self.print(f"[yellow]{msg}[/yellow]")
+                elif record.levelno == logging.DEBUG:
+                    self.print(f"[blue bold]{msg}[/blue bold]")
+                elif record.levelno == logging.ERROR:
+                    self.print(f"[bold red]{msg}[/bold red]")
+                else:
+                    self.print(msg)
+            else:
+                self.print(msg)
+        except Exception:
+            self.handleError(record)
