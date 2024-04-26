5510c232e58bf2090ba76b3cea600ebbe0ee0839
Arthur Camara <camara@zeta-alpha.com>
2024-04-26 16:16:51 +0200

    renaming file

--- ragelo/evaluators/answer_evaluators/__init__.py
+++ ragelo/evaluators/answer_evaluators/__init__.py
@@ -6,6 +6,6 @@ from ragelo.evaluators.answer_evaluators.base_answer_evaluator import (
 from ragelo.evaluators.answer_evaluators.custom_prompt_evaluator import (
     CustomPromptEvaluator,
 )
-from ragelo.evaluators.answer_evaluators.pairwise_reasoning_evaluator import (
+from ragelo.evaluators.answer_evaluators.pairwise_evaluator import (
     PairwiseAnswerEvaluator,
 )
