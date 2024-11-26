7832ea450aefc19581f67b0f6f2eed6aff62a89d
Arthur Camara <camara@zeta-alpha.com>
2024-11-26 14:55:03 +0100

    Continue using LLMResponseType, move pbar to utils, add rich print back to experiment

--- ragelo/utils.py
+++ ragelo/utils.py
@@ -1,9 +1,14 @@
 from __future__ import annotations
 
 import asyncio
+import warnings
 from collections.abc import Callable
 from concurrent.futures import ThreadPoolExecutor
 from typing import Any, Coroutine
+
+from tqdm import TqdmExperimentalWarning
+from tqdm.auto import tqdm
+from tqdm.rich import tqdm_rich
 
 
 def run(coroutine: Coroutine[Any, Any, Any]) -> Any:
@@ -27,3 +32,13 @@ def call_async_fn(fn: Callable[..., Coroutine[Any, Any, Any]], *args: Any, **kwa
     except RuntimeError:
         # If no running loop is detected, run the coroutine directly
         return asyncio.run(fn(*args, **kwargs))
+
+
+def get_pbar(
+    total: int, use_rich: bool = True, ncols: int = 100, desc: str = "Evaluating", disable: bool = False
+) -> tqdm | tqdm_rich:
+    if use_rich:
+        warnings.filterwarnings("ignore", category=TqdmExperimentalWarning)
+        return tqdm_rich(total=total, ncols=ncols, desc=desc, disable=disable)
+    else:
+        return tqdm(total=total, ncols=ncols, desc=desc, disable=disable)
