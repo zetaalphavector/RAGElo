0754ca24e995e2bb633edcfcbca7fbda7ba17364
Arthur Camara <camara@zeta-alpha.com>
2024-11-28 15:31:16 +0100

    remove unused file

--- ragelo/types/console_writer.py
+++
@@ -1,9 +0,0 @@
-"""A class for handling the CLI output with rich printing"""
-
-
-class ConsoleWriter:
-    def __init__(self, use_rich: bool = True):
-        self.use_rich = use_rich
-
-    def write(self, msg: str):
-        print(msg)
