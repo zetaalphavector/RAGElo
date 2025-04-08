a31ec6b637b6929621d5ec77c10a2980fa9ef5df
Arthur Camara <camara@zeta-alpha.com>
2025-04-08 21:14:38 +0200

    Improve logger when rich is not supposed to be used

--- ragelo/logger.py
+++ ragelo/logger.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 import logging
 
 from rich import print as rich_print
@@ -8,13 +10,29 @@ logger = logging.getLogger(__name__)
 class CLILogHandler(logging.Handler):
     """A logging handler for the CLI use case"""
 
+    def __init__(self, use_rich: bool = True, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.use_rich = use_rich
+
     def emit(self, record):
         msg = self.format(record)
         if record.levelno == logging.WARNING:
-            rich_print(f"[yellow]{msg}[/yellow]")
+            if self.use_rich:
+                rich_print(f"[yellow]{msg}[/yellow]")
+            else:
+                print("[WARNING] " + msg)
         elif record.levelno == logging.DEBUG:
-            rich_print(f"[blue bold]{msg}[/blue bold]")
+            if self.use_rich:
+                rich_print(f"[blue bold]{msg}[/blue bold]")
+            else:
+                print("[DEBUG] " + msg)
         elif record.levelno == logging.ERROR:
-            rich_print(f"[bold red]{msg}[/bold red]")
+            if self.use_rich:
+                rich_print(f"[bold red]{msg}[/bold red]")
+            else:
+                print("[ERROR] " + msg)
         else:
-            rich_print(msg)
+            if self.use_rich:
+                rich_print(msg)
+            else:
+                print(msg)
