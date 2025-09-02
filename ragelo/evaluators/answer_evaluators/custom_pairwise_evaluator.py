1538be989318b1f491e16430ac5d014c7140c41d
Arthur Camara <camara@zeta-alpha.com>
2025-09-02 10:45:01 +0200

    docstring, cleanup custom_prompt

--- ragelo/evaluators/answer_evaluators/custom_pairwise_evaluator.py
+++ ragelo/evaluators/answer_evaluators/custom_pairwise_evaluator.py
@@ -1,8 +1,4 @@
-from ragelo.evaluators.answer_evaluators.base_answer_evaluator import (
-    AnswerEvaluatorFactory,
-    BaseAnswerEvaluator,
-)
-from ragelo.llm_providers.base_llm_provider import BaseLLMProvider
+from ragelo.evaluators.answer_evaluators.base_answer_evaluator import AnswerEvaluatorFactory, BaseAnswerEvaluator
 from ragelo.types.configurations import CustomPairwiseEvaluatorConfig
 from ragelo.types.types import AnswerEvaluatorTypes
 
@@ -12,8 +8,3 @@ class CustomPairwiseEvaluator(BaseAnswerEvaluator):
     """A custom pairwise evaluator that allows for additional customization."""
 
     config: CustomPairwiseEvaluatorConfig
-
-    def __init__(self, config: CustomPairwiseEvaluatorConfig, llm_provider: BaseLLMProvider):
-        super().__init__(config, llm_provider)
-        self.system_prompt = config.system_prompt
-        self.user_prompt = config.user_prompt
